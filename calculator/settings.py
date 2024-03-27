c1f3d5e13abca6eb9f7d7014c3f65ae57528de39
annakuchko <66664241+annakuchko@users.noreply.github.com>
2024-03-27 13:22:46 +0000

    Update settings.py

--- calculator/settings.py
+++ calculator/settings.py
@@ -16,7 +16,7 @@ import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
+DISABLE_COLLECTSTATIC = 1
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles") 
 STATICFILES_STORAGE='whitenoise.django.GzipManifestStaticFilesStorage'
 # Quick-start development settings - unsuitable for production
